c417a06911cd9f5e661c3b755341f36b9ae24121
AntonioAnnes <123573311+AntonioAnnes@users.noreply.github.com>
2023-02-28 10:30:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $('#cpf').mask('000.000.000-00')
-    $('#cep').mask('(00000-000')
+    $('#cep').mask('00000-000')
     
     $('form').validate({
         rules: {
